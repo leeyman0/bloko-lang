018c6f289e2a0edfdeea809e0d38f97e2fd98a76
Stann Leey <stannleey0@gmail.com>
2022-12-24 10:34:00 -0500

    removed JavaScript for real this time

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Lexer_1 = require("./Lexer");
-let input = "aushf32342 9f23 <t> 239042n @*(#&$*( )(*)(&@*(#&{}@#}{:";
-let lexer = new Lexer_1.Lexer(input);
-for (let t of lexer) {
-    console.log((0, Lexer_1.tokenToString)(t));
-}
