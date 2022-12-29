8a32b143eb7759bb02693e7b2d9a78c928ce61ad
Stan Leey <stanleey0@gmail.com>
2022-12-29 14:09:58 -0500

    Added positions to tokens

--- Lexer.ts
+++ Lexer.ts
@@ -14,6 +14,8 @@ export type TokenType =
 export type Token = {
     value: string,
     ttype: TokenType,
+    row: number,
+    col: number,
 };
 // punctuation is syntactical
 // punctuation is one of the following characters:
@@ -51,17 +53,19 @@ dictionary.set("Num", num);
 
 // Simple helper function that prints the value of the token 
 export function tokenToString(t: Token): string {
-    return `${t.ttype}\t[${t.value}]`
+    return `${t.ttype}\t[${t.value}]\t(${t.row}, ${t.col})`
 }
 // The main lexer class
 export class Lexer {
     source: string;   // Is the source string 
     right: string;    // Is the string that we have yet to process
-    position: number; // Is the index
+    line: number; // Is the index of the line that
+    col: number;
     constructor(input: string) {
         this.source = input;
         this.right = this.source;
-        this.position = 0;
+        this.line = 1;
+	this.col = 1;
     }
 
     // Getting the next token, or null if it is the end of the file
@@ -70,16 +74,36 @@ export class Lexer {
         if (this.right === "")
             return null;
 
+	// Saving the current row and column
+	let [token_row, token_col]: [number, number] = [this.line, this.col];
+
         // Try matching each of the patterns in turn to what is in the right
         let m: Token | undefined = undefined;
-        for (const [key, val] of dictionary) {
-            let a: Array<string> | null = null;
-            if ((a = val.exec(this.right))) {
+        for (const [key, val] of dictionary) { // This will iterate through the
+	    // token dictionary, in the order that the pairs were added.
+            let a: Array<string> | null = null; // The variable a is how we are
+	    // storing the match
+            if ((a = val.exec(this.right))) { // If there is a match found
+		// Update the string by which we match tokens 
                 this.right = this.right.slice(a[0].length);
+		// Update the line and column
+		this.col += a[0].length;
+		if (key === "Ws" || key === "Str") {
+		    // These types can contain newlines
+		    let lines = a[0].split("\n");
+		    if (lines.length > 1) {// If there are newlines
+			this.line += lines.length - 1;
+			this.col = lines[lines.length - 1].length;
+		    }
+		}
+		// make the token
                 m = {
                     ttype: key,
                     value: a[0],
+		    row: token_row,
+		    col: token_col,
                 };
+		// We don't need to match anything anymore, so break
                 break;
             }
         }
@@ -88,9 +112,12 @@ export class Lexer {
         if (m === undefined) {
             let u = this.right[0];
             this.right = this.right.slice(1);
+	    this.col += 1;
             return {
                 ttype: "Unk",
                 value: u,
+		row: token_row,
+		col: token_col,
             };
         } else return m;
     }
@@ -103,6 +130,8 @@ export class Lexer {
         yield {
             value: "EOF",
             ttype: "End",
+	    row: -1,
+	    col: -1,
         };
     }
 }
